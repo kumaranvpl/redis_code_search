bd2f6f893bdc2b403d9947c48352cb3812ac59b6
Kumaran Rajendhiran <kumaranvpl@gmail.com>
2016-11-09 17:08:37 +0530

    Update file search logic

--- code_search.py
+++ code_search.py
@@ -20,12 +20,19 @@ class CodeSearch:
         files_list = self._get_all_files_on_path(self.redis_path)
 
         for files in files_list:
-            if search_word in open(files).read():
-                print files
+            """if search_word in open(files).read():
+                print files"""
+            with open(files, "r") as f:
+                searchlines = f.readlines()
+            for i, line in enumerate(searchlines):
+                if search_word in line:
+                    print "Keyword %s found in file %s on line number %s" % (search_word, files, i+1)
+                    for l in searchlines[i:i + 3]: print l,
+                    print
 
 
 if __name__ == "__main__":
-    print "hello"
+    #print "hello"
     search_word = str(sys.argv[1])
     redis_search = CodeSearch()
     redis_search.redis_search(search_word)
