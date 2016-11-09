8c94309fe762b29e0374c02585da4f701b956f20
Kumaran Rajendhiran <kumaranvpl@gmail.com>
2016-11-09 23:00:51 +0530

    Add function search

--- code_search.py
+++ code_search.py
@@ -11,31 +11,51 @@ class CodeSearch:
     def _get_all_files_on_path(self, path):
         files_list = []
         for root, directories, filenames in os.walk(path):
-            #for directory in directories:
+            # for directory in directories:
             #    print os.path.join(root, directory)
             for filename in filenames:
                 files_list.append(os.path.join(root, filename))
         return files_list
 
-    def redis_search(self, search_word):
+    def _search_word_in_file(self, filename, word):
+        """if search_word in open(files).read():
+                        print files"""
+        with open(filename, "r") as f:
+            searchlines = f.readlines()
+        for i, line in enumerate(searchlines):
+            if word in line:
+                print "Keyword '%s' found in file '%s' on line number %s" % (word, filename, i + 1)
+                for l in searchlines[i:i + 3]: print l,
+                print
+
+    def redis_search(self, search_word, part_of=None):
         files_list = self._get_all_files_on_path(self.redis_path)
 
+        if part_of in ["func", "function"]:
+            search_word = " " + search_word + "("
+
         for files in files_list:
-            """if search_word in open(files).read():
-                print files"""
-            with open(files, "r") as f:
-                searchlines = f.readlines()
-            for i, line in enumerate(searchlines):
-                if search_word in line:
-                    print "Keyword '%s' found in file '%s' on line number %s" % (search_word, files, i+1)
-                    for l in searchlines[i:i + 3]: print l,
-                    print
+            self._search_word_in_file(filename=files, word=search_word)
+
+
+def do_search(word, part_of=None):
+    if part_of:
+        if part_of not in ["func", "function", "param", "parameter", "var", "variable"]:
+            print "part_of has unsupported value - '%s'. converting it to None" % part_of
+            print "Supported values are func/function, param/parameter, var/variable"
+            part_of = None
+    redis_search = CodeSearch()
+    start_time = time.time()
+    redis_search.redis_search(word, part_of)
+    print "Search took %s seconds" % (time.time() - start_time)
 
 
 if __name__ == "__main__":
     #print "hello"
-    search_word = str(sys.argv[1])
-    redis_search = CodeSearch()
-    start_time = time.time()
-    redis_search.redis_search(search_word)
-    print "Search took %s seconds" % (time.time() - start_time)
+    s_word = str(sys.argv[1])
+    if sys.argv[2]:
+        prt_of = str(sys.argv[2])
+        do_search(s_word, part_of=prt_of)
+    else:
+        do_search(s_word)
+
